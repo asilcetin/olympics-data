8c76c7bcaa9334043295641b76e3426554881903
Asil Çetin <asilcetin@users.noreply.github.com>
2017-11-06 15:54:57 +0100

    cleaning up the repo

--- analysis.R
+++ analysis.R
@@ -1,20 +1,5 @@
 ### Analyzing 2012 London Olympics Data ###
-### by Asil Cetin ###
-
-# First we save our working directory path
-projectWD <- "/Users/asilcetin/Google\ Drive/Development/localhost/uni/bi2/olympics-data"
-
-# Set the working directory
-setwd(projectWD)
-
-# Install readxl, uncomment the following line if not installed already
-# install.packages("readxl")
-
-# Load readxl
-library("readxl")
-
-# Load the data
-OlympicsData <- read_excel("OlympicsData.xlsx")
+### Author: Asil Cetin ###
 
 # Attach the data
 attach(OlympicsData)
@@ -24,4 +9,3 @@ OlympicsData <- OlympicsData[order(-BordaPoints),]
 
 # Distribution of Borda Points by Population (ln)
 plot(`Ln(PopnSize)`, BordaPoints, main="Distribution of Borda Points by Population (ln)", cex=0.5, cex.main=0.5, cex.lab=0.5, cex.axis=0.5, font.main=4, font.lab=2, font.axis=3, col=3, pch=1)
-
